e092244168060bc048ac465f553e69e03f66320d
rajeshmani <rajesh11leoboy@gmail.com>
2020-03-21 18:21:43 +0530

    hbs issue

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname+'/views/partials');
-app.set('view engine','hbs');
+app.set('view engine','.hbs');
 app.use(express.static(__dirname+'/public'));
 
 
